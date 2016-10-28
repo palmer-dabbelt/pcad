ad2be505e25ee31043ca8980c3e62af5353c4ae4
Palmer Dabbelt <palmer@dabbelt.com>
2016-10-28 14:47:58 -0700

    Add tuple adapters for someptr

--- src/simple_match/implementation/someptr.hpp
+++ src/simple_match/implementation/someptr.hpp
@@ -7,6 +7,7 @@
 #include <type_traits>
 
 namespace simple_match {
+	/* Matches a shared_ptr that can be cast to the given type. */
 	template <typename cast_to>
 	struct someptr {
 		template <class T>
@@ -28,6 +29,51 @@ namespace simple_match {
 			}
 		};
 	}
+
+	/* Matches a shared pointer that isn't null. */
+	struct anyptr_t {
+		template <class T>
+		auto get(const std::shared_ptr<T>& t) {
+			return t;
+		}
+	};
+	const static auto anyptr = anyptr_t();
+
+	namespace customization {
+		template <typename cast_from>
+		struct matcher<cast_from, anyptr_t> {
+			static bool check(const cast_from& t, const anyptr_t& v) {
+				return t != nullptr;
+			}
+
+			static auto get(const cast_from& t, const anyptr_t& v) {
+				return std::tie(t);
+			}
+		};
+	}
+
+	/* Matches a shared pointer that is null. */
+	struct noneptr_t {
+		template <class T>
+		auto get(const std::shared_ptr<T>& t) {
+			return t;
+		}
+	};
+	const static auto noneptr = noneptr_t();
+
+	namespace customization {
+		template <typename cast_from>
+		struct matcher<cast_from, noneptr_t> {
+			static bool check(const cast_from& t, const noneptr_t& v) {
+				return t == nullptr;
+			}
+
+			static auto get(const cast_from& t, const noneptr_t& v) {
+				return std::tie();
+			}
+		};
+	}
+
 }
 			
 #endif
